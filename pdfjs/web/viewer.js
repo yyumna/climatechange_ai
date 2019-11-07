5aff9ac2d006d591b06fc733ea6ed931f6e9a385
Andrew Ross <andrewslavinross@gmail.com>
2019-11-07 15:00:35 -0500

    Always reload to top of paper

--- pdfjs/web/viewer.js
+++ pdfjs/web/viewer.js
@@ -13964,7 +13964,7 @@ function getDefaultPreferences() {
       "spreadModeOnLoad": -1,
       "textLayerMode": 1,
       "useOnlyCssZoom": false,
-      "viewOnLoad": 0,
+      "viewOnLoad": 1,
       "disableAutoFetch": false,
       "disableFontFace": false,
       "disableRange": false,
